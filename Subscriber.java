b08dff7d33e12890e7c84a6d44eba544b288345d
simonnCastro <61060425+simonnCastro@users.noreply.github.com>
2024-11-28 15:18:18 +0000

    new file:   NewsAgency.java 	new file:   NewsAgencyObserver.java 	new file:   Subscriber.java 	new file:   SubscriberObserver.java

---
+++ Subscriber.java
@@ -0,0 +1,13 @@
+public class Subscriber{
+    private String subscriberName;
+
+    public String getSubscriberName(){
+        return subscriber;
+    }
+
+    public void setSubscriberName(String subscriberName){
+        this.subscriberName = subscriberName;
+    }
+
+    
+}
